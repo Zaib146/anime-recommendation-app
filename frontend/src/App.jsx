a1e9d6368289ad140c6c3043c47920d4232578fc
Zaib146 <zaib14hayat@gmail.com>
2026-07-16 15:59:35 -0500

    Now similar results in frontend displays the source. need to do with watchlist too

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -440,11 +440,22 @@ User sees webpage*/
                 {similarAnimeById[anime.anime_id] &&
                 (
                   <div className = "similar-anime-recommendations-section">
+
+                    {similarAnimeSourceById[anime.anime_id] &&
+                    (
+                      <p>
+                        Similar Anime results are pulled from {similarAnimeSourceById[anime.anime_id]} API
+                      </p>
+                    )
+                    }
+
                     <p>Similar Anime: </p>    {/* this is JSX, so it goes directly in the return */}
+
                     <div className = "similar-anime-recommendations-list">
                       {similarAnimeById[anime.anime_id].map(similarAnime => (<li>{similarAnime}</li>))}   {/* this line is JavaScript, so it must be wrapped inside {} in this return. Loop through all values in the list and display them in a list format
                       For example, if it's ["Bleach", "Naruto"], then display them with bullets on separate lines */}
                     </div>
+
                   </div>
                 )
                 }
